b8e91a55b5e7425bb68921a70d0d8c0322909135
Matthew Anselmo <matthewanselmo@outlook.com>
2014-11-27 17:28:07 -0800

    radnom

--- ThortonSOAService/ThortonSOAService/Program.cs
+++ ThortonSOAService/ThortonSOAService/Program.cs
@@ -11,23 +11,31 @@ namespace ThortonSOAService
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
-        {
+        {     
+            //Service Start
             logger.Log(LogLevel.Info, "==================================================================\n");
             logger.Log(LogLevel.Info, "Team\t: FunnyGlasses (Matt, Adrian, Kyle)\n");
             logger.Log(LogLevel.Info, "Tag-Name\t: GIORP-TOTAL\n");
             logger.Log(LogLevel.Info, "Service\t: ThortonSOAService\n");
             logger.Log(LogLevel.Info, "==================================================================\n");
             logger.Log(LogLevel.Info, "---\n");
+            
             string command = "";
-            //Service Start
             
             //Register team
+            logger.Log(LogLevel.Info, "Calling SOA-Registry with message :\n");
             command = "";
             SocketSender.StartClient(command);
-            
+
+            logger.Log(LogLevel.Info, "---\n");
+
             //Publish service
+            logger.Log(LogLevel.Info, "Calling SOA-Registry with message :\n");
             command = "";
             SocketSender.StartClient(command);
+
+            logger.Log(LogLevel.Info, "---\n");
+
             SocketListener.StartListening();
         }
     }
