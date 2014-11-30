42a2f9337b4f2a15192ace018f499303481e0a74
Matthew Anselmo <matthewanselmo@outlook.com>
2014-11-29 20:59:31 -0800

    changed from static

--- HL7Lib/HL7Lib/HL7/Definitions.cs
+++ HL7Lib/HL7Lib/HL7/Definitions.cs
@@ -17,14 +17,14 @@ namespace HL7Lib.HL7
         const char END_SEGMENT = (char)13;
         const char END_MESSAGE = (char)28;
 
-        public static const int SOA_NUM_FIELDS = 5;
-        public static const int DRC_NUM_FIELDS = 4;
-        public static const int INF_NUM_FIELDS = 4;
-        public static const int SRV_NUM_FIELDS = 7;
-        public static const int ARG_NUM_FIELDS = 6;
-        public static const int RSP_NUM_FIELDS = 5;
-        public static const int MCH_NUM_FIELDS = 3;
-        public static const int PUB_NUM_FIELDS = 5;
+        public const int SOA_NUM_FIELDS = 5;
+        public const int DRC_NUM_FIELDS = 4;
+        public const int INF_NUM_FIELDS = 4;
+        public const int SRV_NUM_FIELDS = 7;
+        public const int ARG_NUM_FIELDS = 6;
+        public const int RSP_NUM_FIELDS = 5;
+        public const int MCH_NUM_FIELDS = 3;
+        public const int PUB_NUM_FIELDS = 5;
         
     }
 }
